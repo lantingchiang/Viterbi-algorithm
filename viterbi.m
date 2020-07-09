8316a77ae8649c410370f3b37e638b303d12bb73
Lanting Chiang <49918461+lantingchiang@users.noreply.github.com>
2020-07-09 10:01:55 -0400

    two versions of viterbi alg

---
+++ viterbi.m
@@ -0,0 +1,41 @@
+% Input: A - (n*n) state transition probability matrix
+%        B - (n*m) state observation probability matrix
+%        p - (1*n) starting state probability
+%        w - (1*T) indices associated with observed sequence of states
+% Output: I - (1*T) indices associated with sequence of hidden states that
+%                   give highest probability of producing observed states
+%         maxscore - highest probability score found at time T
+function [I, maxscore] = viterbi(A, B, p, w)
+    n = length(A);
+    T = length(w);
+    score = zeros(n, T);
+    pred = zeros(n, T);
+    I = zeros(1, T);
+    
+    % find starting scores (for output sequence of length 1)
+    for j = 1 : n
+        score(j, 1) = p(j) * B(j, w(1));
+    end
+    
+    % forward phase to find best starting scores
+    for t = 2 : T
+        for j = 1 : n
+            tscore = zeros(1, n);
+            for k = 1 : n
+                tscore(k) = score(k, t-1) * A(k, j) * B(j, w(t));
+            end
+            [maxvalue, index] = max(tscore);
+            score(j, t) = maxvalue;
+            pred(j, t) = index;
+        end
+    end
+    
+    % second phase to retrive the optimal path
+    [maxvalue, index] = max(score(:, T));
+    I(T) = index;
+    maxscore = maxvalue;
+    for t = T : -1 : 2
+        I(t-1) = pred(I(t), t);
+    end            
+end
+
